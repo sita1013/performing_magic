6b2fab7474b495c608c648557d3badd41c77ceb0
sita1013 <seluvaiasariahita@gmail.com>
2025-07-04 14:25:32 +0100

    added superuser

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.models import User
 
-# Create your views here.
+def superuser_required(view_func):
+    return user_passes_test(lambda u: isinstance(u, User) and u.is_superuser)(view_func)
+
